7d4e1ebca6f8bc1a07d99f6a4eaf3a82421ac3fb
gguidone <gennaroguido2002@gmail.com>
2024-11-22 12:46:40 +0100

    prima versione finta di piju

--- ComputeTransitionProbabilities.py
+++ ComputeTransitionProbabilities.py
@@ -38,25 +38,35 @@ def compute_matrix_Piju(Constants):
                         #check where you'd end up WITHOUTH current
                         no_current_i, no_current_j = compute_state_with_input(i,j,l, Constants)
                         #check where the drone ends up if it moves
-                        moved_swan_x, moved_swan_y = compute_state_with_input(i_swan,j_swan,l, Constants)
+                        moved_swan_x, moved_swan_y = (i_swan, j_swan) + drone_position_numpy(i_swan,j_swan,i,j)
                         #check if you end un inside the map
                         if 0 <= no_current_i < Constants.N and 0 <= no_current_j < Constants.M:
                             #get the state you'd end up WITHOUTH current as an index
-                            map_j = state2idx(no_current_i,no_current_j,i_swan, j_swan, Constants)
+                            #map_j = state2idx(no_current_i,no_current_j,i_swan, j_swan, Constants)
                             #chek for static collision
-                            no_collisions = 1
-                            if tuple([no_current_i, no_current_j]) in static_drones:
-                                #if hitted, you go home with a probability of 1-p_current
-                                P[map_i][map_start][l] = 1 - Constants.CURRENT_PROB[no_current_i][no_current_j]
-                                no_collisions = 0
-                            if :
-                                no_collisions = 0
-                            if no_collisions:
+                            if not tuple([no_current_i, no_current_j]) in static_drones:
+                                #if not hitted, check for swan collision
+
+                                #if the swan is moving and is not going to hit the drone
+                                if moved_swan_x != no_current_i and moved_swan_y != no_current_j:
+                                    #if no problem arises, you go to the designated x with probability p_current
+                                    P[map_i][state2idx(no_current_i,no_current_j,moved_swan_x, moved_swan_y, Constants)][l] = (1 - Constants.CURRENT_PROB[no_current_i][no_current_j]) * Constants.SWAN_PROB
+                                #the swan is moving and hits the drone
+                                else: 
+                                    P[map_i][map_start][l] = (1 - Constants.CURRENT_PROB[no_current_i][no_current_j]) * Constants.SWAN_PROB
+                                #if the swan is not moving and is not going to hit the drone
+                                if i_swan != no_current_i and j_swan != no_current_j:
+                                    P[map_i][state2idx(no_current_i,no_current_j,i_swan, j_swan, Constants)][l] = (1 - Constants.CURRENT_PROB[no_current_i][no_current_j]) * (1- Constants.SWAN_PROB)
+                                #the swan is not moving and hits the drone
+                                else:
+                                    P[map_i][map_start][l] = (1 - Constants.CURRENT_PROB[no_current_i][no_current_j]) * (1- Constants.SWAN_PROB)
+                            else:
                                 #if no probelm arises: you go to the designated x with probability 1-p_current
-                                P[map_i][map_j][l] = 1 - Constants.CURRENT_PROB[no_current_i][no_current_j]
+                                P[map_i][map_start][l] = 1 - Constants.CURRENT_PROB[no_current_i][no_current_j]
                         else:
                             #if you are outside the map, you go home with probability 1-p_current
                             P[map_i][map_start][l] = 1 - Constants.CURRENT_PROB[no_current_i][no_current_j]
+                            # TODO Il cigno deve respawnare ???
                         # ––––– apply current –-------
                         #check wherer you'd end up WITH current
                         current_i, current_j = compute_state_plus_currents(no_current_i,no_current_j, Constants)
@@ -64,18 +74,28 @@ def compute_matrix_Piju(Constants):
                         path = bresenham((i, j), (current_i, current_j))
                         #check if you end up outside the map
                         if 0 <= current_i < Constants.N and 0 <= current_j < Constants.M:
-                            #convert to index the state
-                            map_j = state2idx(current_i,current_j,i_swan, j_swan, Constants)
                             #check if collision with static drones
-                            if any(tuple(point) in static_drones for point in path):
-                                #if yes go home 
-                                P[map_i][map_start][l] = Constants.CURRENT_PROB[no_current_i][no_current_j]
+                            if not any(tuple(point) in static_drones for point in path):
+                                #if the swan is moving and is not going to hit the drone
+                                if all(point != (moved_swan_x, moved_swan_y) for point in path):
+                                    #if no problem arises, you go to the designated x with probability p_current
+                                    P[map_i][state2idx(current_i,current_j,moved_swan_x, moved_swan_y, Constants)][l] = (Constants.CURRENT_PROB[no_current_i][no_current_j]) * Constants.SWAN_PROB
+                                #the swan is moving and hits the drone
+                                else: 
+                                    P[map_i][map_start][l] = (Constants.CURRENT_PROB[no_current_i][no_current_j]) * Constants.SWAN_PROB
+                                #if the swan is not moving and is not going to hit the drone
+                                if all(point != (i_swan, j_swan) for point in path):
+                                    P[map_i][state2idx(current_i,current_j,i_swan, j_swan, Constants)][l] = (Constants.CURRENT_PROB[no_current_i][no_current_j]) * (1- Constants.SWAN_PROB)
+                                #the swan is not moving and hits the drone
+                                else:
+                                    P[map_i][map_start][l] = (Constants.CURRENT_PROB[no_current_i][no_current_j]) * (1- Constants.SWAN_PROB)
                             else:
                                 #if no, go j
-                                P[map_i][map_j][l] = Constants.CURRENT_PROB[no_current_i][no_current_j]
+                                P[map_i][map_start][l] = Constants.CURRENT_PROB[no_current_i][no_current_j]
                         # if you are outside the map you go home.
                         else:
                             P[map_i][map_start][l] = Constants.CURRENT_PROB[no_current_i][no_current_j]
+                            # TODO Il cigno deve respawnare ???
     return P
 
 def compute_transition_probabilities(Constants):
